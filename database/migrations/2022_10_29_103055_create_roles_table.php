0154d664b1973823d5e790247d9acbebf561ad02
adarsh467 <115977615+adarsh467@users.noreply.github.com>
2022-11-03 12:04:46 +0530

    Role files

---
+++ database/migrations/2022_10_29_103055_create_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('status')->comment("'0' = 'Inactive', '1' = 'Active'")->nullable();
+            $table->dateTime('updated_at')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+            $table->dateTime('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
